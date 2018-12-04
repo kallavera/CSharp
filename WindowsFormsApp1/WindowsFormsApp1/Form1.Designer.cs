005564828ddeda6be394ad0705a6a843e3f524f9
Ariel Velasco <arield.velasco@gmail.com>
2018-12-04 02:29:50 -0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.rdbHombre = new System.Windows.Forms.RadioButton();
+            this.rdbMujer = new System.Windows.Forms.RadioButton();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtRecibo = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(22, 91);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(22, 117);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(47, 13);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido:";
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(22, 143);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(35, 13);
+            this.lblEdad.TabIndex = 2;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // rdbHombre
+            // 
+            this.rdbHombre.AutoSize = true;
+            this.rdbHombre.Location = new System.Drawing.Point(25, 182);
+            this.rdbHombre.Name = "rdbHombre";
+            this.rdbHombre.Size = new System.Drawing.Size(62, 17);
+            this.rdbHombre.TabIndex = 3;
+            this.rdbHombre.TabStop = true;
+            this.rdbHombre.Text = "Hombre";
+            this.rdbHombre.UseVisualStyleBackColor = true;
+            this.rdbHombre.CheckedChanged += new System.EventHandler(this.rdbHombre_CheckedChanged);
+            // 
+            // rdbMujer
+            // 
+            this.rdbMujer.AutoSize = true;
+            this.rdbMujer.Location = new System.Drawing.Point(126, 182);
+            this.rdbMujer.Name = "rdbMujer";
+            this.rdbMujer.Size = new System.Drawing.Size(51, 17);
+            this.rdbMujer.TabIndex = 4;
+            this.rdbMujer.TabStop = true;
+            this.rdbMujer.Text = "Mujer";
+            this.rdbMujer.UseVisualStyleBackColor = true;
+            this.rdbMujer.CheckedChanged += new System.EventHandler(this.rdbMujer_CheckedChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(77, 88);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.LostFocus += new System.EventHandler(this.txtNombre_LostFocus);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(77, 114);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 6;
+            this.txtApellido.LostFocus += new System.EventHandler(this.txtApellido_LostFocus);
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(77, 140);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 7;
+            this.txtEdad.LostFocus += new System.EventHandler(this.txtEdad_LostFocus);
+            // 
+            // txtRecibo
+            // 
+            this.txtRecibo.Location = new System.Drawing.Point(224, 88);
+            this.txtRecibo.Multiline = true;
+            this.txtRecibo.Name = "txtRecibo";
+            this.txtRecibo.Size = new System.Drawing.Size(164, 111);
+            this.txtRecibo.TabIndex = 8;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(224, 205);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(164, 23);
+            this.btnGenerar.TabIndex = 9;
+            this.btnGenerar.Text = "Generar recibo";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtRecibo);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.rdbMujer);
+            this.Controls.Add(this.rdbHombre);
+            this.Controls.Add(this.lblEdad);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.RadioButton rdbHombre;
+        private System.Windows.Forms.RadioButton rdbMujer;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtRecibo;
+        private System.Windows.Forms.Button btnGenerar;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string recibo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //genera el recibo al hacer click en el boton
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+
+        /*
+         *
+         *                            ATENCION!!!
+         * 
+         * hay que agregar el evento lostFocus en el archivo Form.Designer.cs
+         * 
+         *
+         */
+         
+        // Genera un recibo cada vez que algun txtbox pierde el foco
+        private void txtNombre_LostFocus(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+        private void txtApellido_LostFocus(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+
+        private void txtEdad_LostFocus(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+
+        // genera el recibo cada vez que se hace click en el radioButton
+
+        private void rdbHombre_CheckedChanged(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+
+        private void rdbMujer_CheckedChanged(object sender, EventArgs e)
+        {
+            procesarDatos();
+        }
+
+        //esta funcion es la que se encarga de verificar cuales textbox se encuentran con datos para generar el recibo con los datos que correspondan
+        private void procesarDatos()
+        {
+            recibo = "Datos del cliente" + System.Environment.NewLine;
+            recibo += "=================" + System.Environment.NewLine;
+
+            if(txtNombre.Text != "")
+            {
+                recibo += "Nombre: " + txtNombre.Text + System.Environment.NewLine;
+            }
+
+            if (txtApellido.Text != "")
+            {
+                recibo += "Apellido: " + txtApellido.Text + System.Environment.NewLine;
+            }
+
+            if (txtEdad.Text != "")
+            {
+                recibo += "Edad: " + txtEdad.Text + System.Environment.NewLine;
+            }
+
+            if(rdbHombre.Checked)
+            {
+                recibo += "Sexo: Hombre" + System.Environment.NewLine;
+            }
+            else if (rdbMujer.Checked)
+            {
+                recibo += "Sexo: Mujer" + System.Environment.NewLine;
+            }
+
+            txtRecibo.Text = recibo;
+        }
+    }
+}
